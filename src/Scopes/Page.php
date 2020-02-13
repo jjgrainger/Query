098922027fb7353b071fb918fcbfc7b90ba6c056
jjgrainger <github@jjgrainger.co.uk>
2020-02-13 17:11:35 +0000

    add page scope

---
+++ src/Scopes/Page.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Query\Scopes;
+
+use Query\Scope;
+use Query\Builder;
+
+class Page implements Scope
+{
+    /**
+     * Aliases for the scope.
+     *
+     * @var array
+     */
+    public $aliases = [
+        'page_id',
+        'pagename',
+    ];
+
+    /**
+     * Apply the scope to the query builder.
+     *
+     * @param  Builder $builder
+     * @param  mixed   $limit
+     *
+     * @return Builder
+     */
+    public function apply(Builder $builder, $page = null)
+    {
+        $key = is_int($page) ? 'page_id' : 'pagename';
+
+        return $builder->where($key, $page);
+    }
+}
